0b152df007a1cf4dd9c722f9ca0e3fcbcb02d988
Francisco Costa <frcosta@itba.edu.ar>
2025-06-10 20:44:14 -0300

    chore: tp finalizado?

--- x64BareBones/compile.sh
+++ x64BareBones/compile.sh
@@ -1,3 +1,17 @@
 docker start TPE
+
+# Limpiar todo
+docker exec -it TPE make clean -C /root/Toolchain
+docker exec -it TPE make clean -C /root/
+
+# Construir Toolchain
 docker exec -it TPE make all -C /root/Toolchain
+
+# Construir todo el proyecto
 docker exec -it TPE make all -C /root/
+
+sudo chown -R $USER:$USER .
+sudo chmod -R 755 .
+
+# Verificar que se crearon los archivos necesarios
+ls -la Image/x64BareBonesImage.*
